87576af7007aa90fba115167eae91258651f5c0d
Ben Rutland <benrutland@hotmail.com>
2021-05-11 20:36:10 +0100

    updated clear:cache command to use custom flush instead of wp transient command

--- src/Cache/Console/CacheClear.php
+++ src/Cache/Console/CacheClear.php
@@ -2,6 +2,7 @@
 
 namespace Radiate\Cache\Console;
 
+use Radiate\Cache\Repository;
 use Radiate\Console\Command;
 
 class CacheClear extends Command
@@ -21,12 +22,38 @@ class CacheClear extends Command
     protected $description = 'Flush the application cache';
 
     /**
+     * The cache instance.
+     *
+     * @var \Radiate\Cache\Repository
+     */
+    protected $cache;
+
+    /**
+     * Create a new cache clear command instance.
+     *
+     * @param  \Radiate\Cache\Repository  $cache
+     * @return void
+     */
+    public function __construct(Repository $cache)
+    {
+        parent::__construct();
+
+        $this->cache = $cache;
+    }
+
+    /**
      * Execute the console command.
      *
      * @return void
      */
     public function handle()
     {
-        $this->call('transient delete --all');
+        $successful = $this->cache->flush();
+
+        if (!$successful) {
+            return $this->error('Failed to clear cache. Make sure you have the appropriate permissions.');
+        }
+
+        $this->info('Application cache cleared!');
     }
 }
