c40c85a19175d7ccefafbb0e32371a6a1a3ea14b
Ben Rutland <benrutland@hotmail.com>
2021-05-13 18:29:23 +0100

    added methods to user for authorization

--- src/Database/Models/User.php
+++ src/Database/Models/User.php
@@ -6,6 +6,7 @@ use DateTimeImmutable;
 use Radiate\Database\Concerns\HasMeta;
 use Radiate\Database\Model;
 use Radiate\Database\UserQueryBuilder;
+use Radiate\Support\Facades\Gate;
 
 class User extends Model
 {
@@ -259,5 +260,53 @@ class User extends Model
     public function capabilities()
     {
         return get_userdata($this->getKey())->allcaps;
+    }
+
+    /**
+     * Determine if the entity has the given abilities.
+     *
+     * @param  array|string  $abilities
+     * @param  array|mixed  $arguments
+     * @return bool
+     */
+    public function can($abilities, $arguments = [])
+    {
+        return Gate::forUser($this)->check($abilities, $arguments);
+    }
+
+    /**
+     * Determine if the entity has any of the given abilities.
+     *
+     * @param  array|string  $abilities
+     * @param  array|mixed  $arguments
+     * @return bool
+     */
+    public function canAny($abilities, $arguments = [])
+    {
+        return Gate::forUser($this)->any($abilities, $arguments);
+    }
+
+    /**
+     * Determine if the entity does not have the given abilities.
+     *
+     * @param  array|string  $abilities
+     * @param  array|mixed  $arguments
+     * @return bool
+     */
+    public function cant($abilities, $arguments = [])
+    {
+        return !$this->can($abilities, $arguments);
+    }
+
+    /**
+     * Determine if the entity does not have the given abilities.
+     *
+     * @param  array|string  $abilities
+     * @param  array|mixed  $arguments
+     * @return bool
+     */
+    public function cannot($abilities, $arguments = [])
+    {
+        return $this->cant($abilities, $arguments);
     }
 }
