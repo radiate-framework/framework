99f6ab59b3227838806c1f96497c5ffbfbf7e269
Ben Rutland <benrutland@hotmail.com>
2021-05-17 22:02:14 +0100

    removed parameters from stack method

--- src/Routing/Route.php
+++ src/Routing/Route.php
@@ -258,14 +258,14 @@ abstract class Route
      * @param array $parameters
      * @return mixed
      */
-    protected function runRequestThroughStack(Request $request, array $parameters = [])
+    protected function runRequestThroughStack(Request $request)
     {
         try {
             $response = (new Pipeline($this->app))
                 ->send($request)
                 ->through($this->gatherMiddleware())
-                ->then(function ($request) use ($parameters) {
-                    return $this->app->call($this->action(), $parameters);
+                ->then(function () {
+                    return $this->app->call($this->action(), $this->parameters());
                 });
         } catch (Throwable $e) {
             $response = $this->app->renderException($request, $e);
