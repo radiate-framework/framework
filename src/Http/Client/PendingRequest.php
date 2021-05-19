8d390a818fe3c5ec44664a4b3de822ec32764852
Ben Rutland <benrutland@hotmail.com>
2021-05-19 20:07:38 +0100

    added cookie option for pool requests

--- src/Http/Client/PendingRequest.php
+++ src/Http/Client/PendingRequest.php
@@ -383,6 +383,7 @@ class PendingRequest
             'data'    => $options['body'],
             'type'    => $method,
             'options' => $options,
+            'cookies' => $options['cookies'],
         ];
 
         return $this;
