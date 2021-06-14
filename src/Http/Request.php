4b851529bc6af555cc825bea0aa3a67aaccda6fb
Ben Rutland <benrutland@hotmail.com>
2021-06-14 21:45:09 +0100

    added boolean method to request

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -373,6 +373,20 @@ class Request implements ArrayAccess, JsonSerializable
     }
 
     /**
+     * Retrieve input as a boolean value.
+     *
+     * Returns true when value is "1", "true", "on", and "yes". Otherwise, returns false.
+     *
+     * @param  string|null  $key
+     * @param  bool  $default
+     * @return bool
+     */
+    public function boolean(?string $key = null, bool $default = false)
+    {
+        return filter_var($this->get($key, $default), FILTER_VALIDATE_BOOLEAN);
+    }
+
+    /**
      * Determine if the request is secure
      *
      * @return boolean
