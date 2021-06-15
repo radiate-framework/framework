91ba5750eea9d67ec07c9f59faa526499373b940
Ben Rutland <benrutland@hotmail.com>
2021-06-15 22:42:30 +0100

    changed mail to subpath

--- src/Mail/MailServiceProvider.php
+++ src/Mail/MailServiceProvider.php
@@ -30,7 +30,7 @@ class MailServiceProvider extends ServiceProvider
         ]);
 
         $this->publishes([
-            __DIR__ . '/resources/views' => $this->app['config']['view.path'],
+            __DIR__ . '/resources/views' => $this->app['config']['view.path'] . '/mail',
         ], 'mail');
     }
 }
