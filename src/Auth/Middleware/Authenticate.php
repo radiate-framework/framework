7b6f56e4b4c33f05e6ad76555af58b36ab4f4426
Ben Rutland <benrutland@hotmail.com>
2021-06-20 22:07:16 +0100

    adjusted auth middleware

--- src/Auth/Middleware/Authenticate.php
+++ src/Auth/Middleware/Authenticate.php
@@ -3,21 +3,42 @@
 namespace Radiate\Auth\Middleware;
 
 use Closure;
+use Radiate\Auth\AuthManager;
 use Radiate\Foundation\Http\Exceptions\HttpResponseException;
 use Radiate\Http\Request;
 
 class Authenticate
 {
     /**
+     * The auth mananger
+     *
+     * @var \Radiate\Auth\AuthManager
+     */
+    protected $auth;
+
+    /**
+     * Create the middeware
+     *
+     * @param \Radiate\Auth\AuthManager $auth
+     */
+    public function __construct(AuthManager $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    /**
      * Handle an incoming request.
      *
      * @param \Radiate\Http\Request $request
      * @param \Closure $next
+     * @param string|null $guard
      * @return mixed
+     *
+     * @throws \Radiate\Foundation\Http\Exceptions\HttpResponseException
      */
-    public function handle(Request $request, Closure $next)
+    public function handle(Request $request, Closure $next, $guard = null)
     {
-        if ($request->user()) {
+        if ($this->auth->guard($guard)->check()) {
             return $next($request);
         }
 
