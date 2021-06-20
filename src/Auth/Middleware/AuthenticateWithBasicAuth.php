cfedd1764e85efd3d56c0b2518bf24b4bf6d7f45
Ben Rutland <benrutland@hotmail.com>
2021-06-20 22:05:48 +0100

    added basic auth middleware

---
+++ src/Auth/Middleware/AuthenticateWithBasicAuth.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Radiate\Auth\Middleware;
+
+use Closure;
+use Radiate\Auth\AuthManager;
+use Radiate\Foundation\Http\Exceptions\HttpResponseException;
+use Radiate\Http\Request;
+
+class AuthenticateWIthBasicAuth
+{
+    /**
+     * The auth mananger
+     *
+     * @var \Radiate\Auth\AuthManager
+     */
+    protected $auth;
+
+    /**
+     * Create the middeware
+     *
+     * @param \Radiate\Auth\AuthManager $auth
+     */
+    public function __construct(AuthManager $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Radiate\Http\Request $request
+     * @param \Closure $next
+     * @param string|null $guard
+     * @param string|null $field
+     * @return mixed
+     *
+     * @throws \Radiate\Foundation\Http\Exceptions\HttpResponseException
+     */
+    public function handle(Request $request, Closure $next, $guard = null, $field = null)
+    {
+        if ($this->auth->guard($guard)->basic($field ?: 'email')) {
+            return $next($request);
+        }
+
+        header('WWW_Authenticate: Basic');
+
+        throw new HttpResponseException('Unauthorised.', 401);
+    }
+}
