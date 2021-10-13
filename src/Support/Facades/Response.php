fc6005ff780e6c829010d5194850df83fff84be3
Ben Rutland <benrutland@hotmail.com>
2021-10-13 22:45:43 +0100

    updated facade

--- src/Support/Facades/Response.php
+++ src/Support/Facades/Response.php
@@ -8,7 +8,7 @@ use Radiate\Http\ResponseFactory;
  * @method static \Radiate\Http\Response make(mixed $content, int $status, array $headers = []) Make a response
  * @method static \Radiate\Http\Response view(string $view, array $data = [], int $status = 200, array $headers = []) Return a view
  * @method static \Radiate\Http\JsonResponse json(mixed $content = [], int $status = 200, array $headers = [], int $options = 0) Return a JSON response
- * @method static \Radiate\Http\Response redirect(string $location, int $status = 302) Redirect responses
+ * @method static \Radiate\Http\RedirectResponse redirect(string $location, int $status = 302) Redirect responses
  *
  * @see \Radiate\Http\Response
  */
