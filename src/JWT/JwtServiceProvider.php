d8b52aa6ce4681ce74237b67791842a66b76bf4f
Ben Rutland <benrutland@hotmail.com>
2021-10-20 23:41:10 +0100

    added jwt macro to request

--- src/JWT/JwtServiceProvider.php
+++ src/JWT/JwtServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace Radiate\JWT;
 
-use Firebase\JWT\JWT;
+use Firebase\JWT\JWT as FirebaseJWT;
 use Radiate\JWT\Manager;
+use Radiate\Support\Facades\JWT;
+use Radiate\Support\Facades\Request;
 use Radiate\Support\ServiceProvider;
 
 class JwtServiceProvider extends ServiceProvider
@@ -16,7 +18,33 @@ class JwtServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->singleton(Manager::class, function ($app) {
-            return new Manager($app, new JWT);
+            return new Manager($app, new FirebaseJWT);
+        });
+    }
+
+    /**
+     * Boot the service provider
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerRequestJwt();
+    }
+
+    /**
+     * Register the "jwt" macro on the request.
+     *
+     * @return void
+     */
+    protected function registerRequestJwt()
+    {
+        Request::macro('jwt', function (?string $claim = null) {
+            if ($claim) {
+                return JWT::payload($this)->$claim;
+            }
+
+            return JWT::payload($this);
         });
     }
 }
